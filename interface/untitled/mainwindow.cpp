1f6fc7a54f557dbd1f88fda06858423fa7de34cc
Ri0S <aa248424@gmail.com>
2017-12-16 10:41:33 +0900

    first

--- interface/untitled/mainwindow.cpp
+++ interface/untitled/mainwindow.cpp
@@ -1,7 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFont>
-
+#include <string>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+using namespace std;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -23,12 +27,30 @@ void MainWindow::on_pushButton_clicked()
     ui->pushButton_3->setEnabled(true);
 }
 
+/*
+string GetHangul(string english)
+{
+    FILE* file = popen("ls", "r");
+    // use fscanf to read:
+    char buffer[2000];
+    fscanf(file, "%2000s", buffer);
+    pclose(file);
+
+
+}
+*/
+
 void MainWindow::on_pushButton_2_clicked()
 {
+    string seed = ui->textEdit_2->toPlainText().toStdString();
+    //system("");
     ui->textEdit->setText(ui->textEdit_2->toPlainText());
+
 //    after recv poem and sound file
 //    start botton activate
 //    send seed to serv
+//    if can't type hangul in program
+//    use heconvert.py with pipe
     ui->pushButton->setDisabled(true);
 //    after receiving done, activate start button
     ui->pushButton->setEnabled(true);
